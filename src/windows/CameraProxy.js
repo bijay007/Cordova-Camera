f87dd114454ed016db25b1e6de157c472e84a8f9
Bijay Timilsina <bjtimi.007@gmail.com>
2017-08-21 10:54:20 +0200

    Change 2 orientation values for 180 and 90

--- src/windows/CameraProxy.js
+++ src/windows/CameraProxy.js
@@ -474,11 +474,11 @@ function takePictureFromCameraWP(successCallback, errorCallback, args) {
                 return Windows.Media.Capture.VideoRotation.none;
             // landscape
             case Windows.Devices.Sensors.SimpleOrientation.rotated90DegreesCounterclockwise:
-                return Windows.Media.Capture.VideoRotation.clockwise180Degrees;
+                return Windows.Media.Capture.VideoRotation.clockwise270Degrees;
             // portrait-flipped (not supported by WinPhone Apps)
             case Windows.Devices.Sensors.SimpleOrientation.rotated180DegreesCounterclockwise:
                 // Falling back to portrait default
-                return Windows.Media.Capture.VideoRotation.clockwise270Degrees;
+                return Windows.Media.Capture.VideoRotation.clockwise180Degrees;
             // landscape-flipped
             case Windows.Devices.Sensors.SimpleOrientation.rotated270DegreesCounterclockwise:
                 return Windows.Media.Capture.VideoRotation.clockwise90Degrees;
